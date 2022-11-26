f4ba82fc1ca1e37f56f261cd4735cf1f593e7853
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-11-26 23:21:23 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stoptBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+// functions
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+// Handlers
+
+let timeoutId = null;
+
+const onBtnClick = e => {
+  if (e.target.type !== 'button') {
+    return;
+  } else if (
+    e.target === refs.startBtn &&
+    !refs.startBtn.classList.contains('active-btn')
+  ) {
+    refs.startBtn.classList.add('active-btn');
+    timeoutId = setInterval(changeBodyColor, 1000);
+  } else if (
+    e.target === refs.stoptBtn &&
+    refs.startBtn.classList.contains('active-btn')
+  ) {
+    refs.startBtn.classList.remove('active-btn');
+    clearTimeout(timeoutId);
+  } else {
+    return;
+  }
+};
+
+// Listeners
+
+refs.body.addEventListener('click', onBtnClick);
