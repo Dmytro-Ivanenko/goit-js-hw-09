c9f44aa70f2e2cdcec354ba773f4170f61e79124
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-11-28 01:53:13 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,68 @@
+import Toastify from 'toastify-js';
+import 'toastify-js/src/toastify.css';
+
+const refs = {
+  formElements: document.querySelector('form').elements,
+  submitBtn: document.querySelector('button'),
+};
+
+const promiseSettings = {
+  delay: '',
+  step: '',
+  amount: '',
+};
+
+// Functions
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function startPromises({ delay, step, amount }) {
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Toastify({
+          text: `Fulfilled promise ${position} in ${delay}ms`,
+          style: {
+            background: '#76d163',
+          },
+          close: true,
+          duration: 10000,
+        }).showToast();
+      })
+      .catch(({ position, delay }) => {
+        Toastify({
+          text: `Rejected promise ${position} in ${delay}ms`,
+          style: {
+            background: '#b92626',
+          },
+          close: true,
+          duration: 10000,
+        }).showToast();
+      });
+    delay += step;
   }
 }
+
+// Handlers
+const onSubmit = e => {
+  e.preventDefault();
+
+  promiseSettings.delay = +document.querySelector('[name = delay]').value;
+  promiseSettings.step = +document.querySelector('[name = step]').value;
+  promiseSettings.amount = +document.querySelector('[name = amount]').value;
+
+  startPromises(promiseSettings);
+};
+
+refs.submitBtn.addEventListener('click', onSubmit);
